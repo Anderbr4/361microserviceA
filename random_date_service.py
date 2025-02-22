237149b15f410b786168840d3f7f09e90b9d121c
Brian Anderson <anderbr4@engr.orst.edu>
2025-02-22 23:50:03 +0000

    work

---
+++ random_date_service.py
@@ -0,0 +1,5 @@
+
+
+
+def generate_random_date():
+    return "01/01/25"
---
+++ random_date_tests.py
@@ -0,0 +1,24 @@
+import unittest
+
+from random_date_service import (
+    generate_random_date
+)
+
+def add(a, b):
+    return a + b
+
+print(generate_random_date())
+
+class TestAddFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(1, 2), 3)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1, -2), -3)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add(1, -2), -1)
+        self.assertEqual(add(-1, 2), 1)
+
+if __name__ == '__main__':
+    unittest.main()
