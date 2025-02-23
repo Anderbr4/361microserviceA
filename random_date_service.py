5e32ad8b1b4d9f31d77c8b14272fcd072dfe54d8
Brian Anderson <anderbr4@engr.orst.edu>
2025-02-23 01:30:05 +0000

    work

--- random_date_service.py
+++ random_date_service.py
@@ -1,17 +1,22 @@
 import random 
-import time 
+import datetime 
 
 
 def generate_random_date():
 
-    rando_month = str(random.randint(1, 12))
-    rando_day = str(random.randint(1, 31))
-    if(rando_month == 2):
+    # actually the rando month day year thing is tough because you may never get leap years/some months have 30 vs. 31 days. 
+    # so having a set day and then adding a random period of time to it seems better. 
 
-        rando_day = str(random.randint(1, 28))
+    # https://www.geeksforgeeks.org/how-to-add-days-to-a-date-in-python/
+    # accessed 02.22.25
+    # specifically referencing the use of timedelta function so I can add days to a date
+    # 
+    start_date = datetime.date.today()
+    days_to_add = random.randint(0,365)
 
-    rando_year = str(random.randint(10, 25))
-    return rando_month+"/"+rando_day+"/"+rando_year
+    rando_date = start_date + datetime.timedelta(days=days_to_add)
+
+    return rando_date
 
 
-print(generate_random_date())
+# print(generate_random_date())
