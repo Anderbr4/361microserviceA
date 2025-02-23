4a9de83eb3dd2ccc6df9fe9bf70e74e60df7a544
Brian Anderson <anderbr4@engr.orst.edu>
2025-02-23 01:15:37 +0000

    work

--- random_date_service.py
+++ random_date_service.py
@@ -1,5 +1,17 @@
-
+import random 
+import time 
 
 
 def generate_random_date():
-    return "01/01/25"
+
+    rando_month = str(random.randint(1, 12))
+    rando_day = str(random.randint(1, 31))
+    if(rando_month == 2):
+
+        rando_day = str(random.randint(1, 28))
+
+    rando_year = str(random.randint(10, 25))
+    return rando_month+"/"+rando_day+"/"+rando_year
+
+
+print(generate_random_date())
--- random_date_tests.py
+++ random_date_tests.py
@@ -4,21 +4,20 @@ from random_date_service import (
     generate_random_date
 )
 
-def add(a, b):
-    return a + b
 
-print(generate_random_date())
+
+
+
 
 class TestAddFunction(unittest.TestCase):
-    def test_add_positive_numbers(self):
-        self.assertEqual(add(1, 2), 3)
+    def test_generate_date_time_1(self):
+        self.assertEqual(generate_random_date(1, 2), 3)
 
-    def test_add_negative_numbers(self):
-        self.assertEqual(add(-1, -2), -3)
+    def test_generate_date_time_2(self):
+        self.assertEqual(generate_random_date(-1, -2), -3)
 
-    def test_add_mixed_numbers(self):
-        self.assertEqual(add(1, -2), -1)
-        self.assertEqual(add(-1, 2), 1)
+    def test_generate_date_time_3(self):
+        self.assertEqual(generate_random_date(1, -2), -1)
 
 if __name__ == '__main__':
     unittest.main()
